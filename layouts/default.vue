a94234bb33791fc599e85bf7f22d69fcb3e9bbe8
phsang <sang.huynhphuoc@mfast.vn>
2025-04-16 05:30:13 +0700

    [yenmarket] update list of items

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,20 @@
   <div class="layout">
     <AppHeader />
     <main class="main-content">
+
+      <div class="list_of_items">
+        <div class="container">
+          <div class="list_item_btn">
+            <button type="button" class="list_of_items__button">
+              <span class="list_items_bar"></span>
+              <span class="list_items_bar"></span>
+              <span class="list_items_bar"></span>
+            </button>
+            <span>LIST OF ITEMS</span>
+          </div>
+        </div>
+      </div>
+
       <div class="container">
         <slot />
       </div>
@@ -23,7 +37,40 @@
 
 .main-content {
   flex: 1;
-  padding: 2rem 0;
+  padding: 0 0 2rem;
+}
+
+.list_of_items {
+  background-color: #ca2628;
+  margin-bottom: 2rem;
+  display: flex;
+  height: 42px;
+}
+
+.list_of_items__button {
+  display: flex;
+  flex-direction: column;
+  align-items: space-around;
+  justify-content: center;
+  background-color: transparent;
+  border: none;
+  padding: 0;
+}
+
+.list_of_items__button .list_items_bar {
+  width: 26px;
+  height: 2px;
+  background-color: #f0f0f0;
+  margin: 2px 0;
+  transition: all 0.3s ease;
+}
+
+.list_item_btn {
+  display: flex;
+  align-items: center;
+  color: #f0f0f0;
+  gap: 12px;
+  margin: 8px 0;
 }
 
 @media (max-width: 768px) {

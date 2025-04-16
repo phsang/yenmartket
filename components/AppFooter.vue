74ddcedf934bbaa05126978ea58667465e380878
phsang <sang.huynhphuoc@mfast.vn>
2025-04-16 07:20:23 +0700

    [yenmarket] update list of items

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -30,6 +30,9 @@
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
             </a>
           </div>
+          <a href="http://online.gov.vn/Home/WebDetails/66187" target="_blank" rel="noopener noreferrer" class="bct-logo">
+            <img src="http://bizweb.dktcdn.net/100/382/166/themes/900578/assets/logo_bct.png?1724210132257" width="140" alt="BCT" />
+          </a>
         </div>
       </div>
       <div class="copyright">
@@ -111,6 +114,11 @@
   color: #6c757d;
   border-top: 1px solid #dee2e6;
   padding-top: 1.5rem;
+}
+
+.bct-logo {
+  display: flex;
+  margin-top: 20px;
 }
 
 @media (max-width: 768px) {
